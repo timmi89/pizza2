d0020731aa4537735b03e6a6586498397c1a7e95
timothea mcguire <timothea.mcguire@gmail.com>
2016-01-29 14:38:55 -0800

    spec broke, then fixed, jquery added

--- js/scripts.js
+++ js/scripts.js
@@ -25,8 +25,43 @@ Pizza.prototype.toppingsCostCalc = function(toppings, pizzaSize) {
     }
     return totalCost;
   }
+  $(document).ready(function() {
+    $("button.submit").click(function(event) {
+      event.preventDefault();
 
-$(document).ready(function() {
+      var grabSelectedToppings = function() {
+        toppingsSelected = [];
+        $("#toppings :checked").each(function() {
+          toppingsSelected.push($(this).val());
+        });
+        return toppingsSelected;
+      };
+
+      var newPizza = new Pizza(toppings, pizzaSize, price);
+      var toppings = grabSelectedToppings();
+      var pizzaSize = $("select.pizzaSize").val();
+      var priceToppings = newPizza.toppingsCostCalc(toppings, pizzaSize);
+      var price = newPizza.pizzaPrice(priceToppings, pizzaSize);
+
+      if (toppings.length === 0) {
+        $(".selectedtoppings").show().append("<span class='pizzaInfo'>" + pizzaSize + "<br>" + "total: $" + price + ".00 <br></span><br>");
+      } else {
+        $(".selectedtoppings").show().append("<span class='pizzaInfo'>" + toppings + "<br>" + pizzaSize + "<br>" + "Total: $" + price + ".00 </span><br>");
+      }
 
 
-});
+    });
+  });
+
+
+  // $( "input" ).on( "click", function() {
+  //   $( ".selectedtoppings" ).text(html( $( "input:checked" ).val() ));
+  // });
+
+// $(document).ready(function() {
+//   $("#order").click(function(event){
+//     event.preventDefault();
+//     var toppings= $("input#topp")
+//   }
+//
+// });
